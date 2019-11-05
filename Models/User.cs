e66c289978421112a7b0e7f79ad418debac0eed7
alisherKak <alisher.alish333@gmail.com>
2019-11-06 01:04:21 +0600

    Home Work 19.11.07

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
